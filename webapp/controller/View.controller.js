7035494982c4da4890cffef721d12a49c1fa89d7
Janet Gloria <janet.gloria@commsult.id>
2023-04-13 14:27:23 +0200

    new buttons and features!

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -98,6 +98,108 @@ sap.ui.define(
           }.bind(this)
         );
       },
+
+      //Fragment and CRUD Controls
+      _getDialog: function () {
+        if (!this._oDialog) {
+          this._oDialog = sap.ui.xmlfragment(
+            "nodebaseui5.view.fragment.AddShipment",
+            this
+          );
+          this.getView().addDependent(this._oDialog);
+        }
+        return this._oDialog;
+      },
+
+      onOpenShipmentFragment: function () {
+        this._getDialog().open();
+      },
+
+
+      closeDialog: function (oEvent) {
+        // if (
+        //   this.getView()
+        //     .getModel("shipmentModel")
+        //     .getContext("/currentShipment")
+        //     .getObject()
+        // ) {
+        //   this.clearAddedShipment(oEvent);
+        // }
+        this._getDialog().close();
+      },
+
+      onSaveShipment: function (oEvent) {
+        var oModel = oEvent.getSource().getModel("shipmentModel");
+        var oShipment = oModel.getContext("/currentShipment").getObject();
+
+        this.collRefShipments
+          .add(oShipment)
+          .then(
+            function (docShipment) {
+              oShipment.id = docShipment.id;
+              oModel.refresh(true);
+              this.getView()
+                .byId("shipmentTable")
+                .getBinding("items")
+                .refresh();
+              this.clearAddedShipment(oEvent);
+              this.closeDialog();
+              this.showMessageToast("Shipment created successfully.");
+            }.bind(this)
+          )
+          .catch(function (error) {
+            console.error("Error adding document: ", error);
+          });
+      },
+
+      onShipmentStatusSelect: function (oEvent) {
+        var oShipment = oEvent
+          .getSource()
+          .getModel("shipmentModel")
+          .getContext("/currentShipment")
+          .getObject();
+        oShipment.status = oEvent.getSource().getSelectedItem().getKey();
+      },
+
+      showMessageToast: function (message) {
+        MessageToast.show(message);
+      },
+
+      onDeleteShipment: function (oEvent) {
+        var me = this;
+        var shipmentId = oEvent.getSource().data().shipment.id;
+        var shipmentCode = oEvent.getSource().data().shipment.code;
+        MessageBox.confirm(
+          "Are you sure you want to delete the shipment: " + shipmentCode + "?",
+          {
+            actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+            onClose: function (sAction) {
+              if (sAction === "YES") {
+                me.collRefShipments
+                  .doc(shipmentId)
+                  .delete()
+                  .then(
+                    function () {
+                      me.getView()
+                        .byId("shipmentTable")
+                        .getBinding("items")
+                        .refresh();
+                      me.showMessageToast(
+                        "Shipment: " + shipmentCode + " deleted successfully."
+                      );
+                    }.bind(me)
+                  )
+                  .catch(function (error) {
+                    console.error(
+                      "Error removing document: " + shipmentCode + ": ",
+                      error
+                    );
+                  });
+              }
+            },
+          }
+        );
+      },
     });
   }
 );
